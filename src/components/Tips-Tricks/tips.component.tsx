7fe07aebbf064b278c152183062bb1a58236fc79
sharleenawinja <awinjasharleen@gmail.com>
2023-05-17 15:30:42 +0300

    creating reusable component for grid items

--- src/components/Tips-Tricks/tips.component.tsx
+++ src/components/Tips-Tricks/tips.component.tsx
@@ -4,6 +4,64 @@ import Sidebar from "../layout/SideBar.component";
 import { currentItems } from "../../data/data";
 import Pagination from "../layout/paginations";
 
+interface SingleArticleProps {
+  index: number;
+  current: {
+    heading: string;
+    details: string;
+    link: string;
+  };
+}
+
+const SingleArticle: React.FC<SingleArticleProps> = ({
+  index,
+  current,
+}: SingleArticleProps) => {
+  return (
+    <div
+      className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
+      key={index}
+    >
+      <a href="#">
+        <img
+          className="rounded-t-lg w-full h-48 object-cover"
+          src={current.link}
+          alt=""
+        />
+      </a>
+      <div className="p-5">
+        <a href="#">
+          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+            {current.heading}
+          </h5>
+        </a>
+        <p className="mb-3 text-sm text-gray-700 dark:text-gray-400">
+          {current.details}
+        </p>
+        <a
+          href="#"
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
+        >
+          Read more
+          <svg
+            aria-hidden="true"
+            className="w-4 h-4 ml-2 -mr-1"
+            fill="currentColor"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              fillRule="evenodd"
+              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
+              clipRule="evenodd"
+            ></path>
+          </svg>
+        </a>
+      </div>
+    </div>
+  );
+};
+
 const Tips = () => {
   // confirm forms used for tb prophylaxis medication  =.QA
   // which encounter types are tied to those forms  => encounter type table etl
@@ -152,53 +210,13 @@ const Tips = () => {
                 </svg>
               </button>
             </div>
-            {/* clean up code, create reusable component for the cards in the grid, find a better place to place add new tips/tricks button eg at the bottom next to pagination, increase height of the form to cover either the complete screen or 2 thirds of the screen */}
+            {/* clean up code, create reusable component for the cards in the grid, find a better place to place add new tips/tricks button eg at the bottom next to pagination*/}
 
             <div className="ml-2 mr-2">
               <div className="w-full mx-auto">
                 <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
                   {itemsToShow.map((current, index) => (
-                    <div
-                      className="bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700"
-                      key={index}
-                    >
-                      <a href="#">
-                        <img
-                          className="rounded-t-lg w-full h-48 object-cover"
-                          src={current.link}
-                          alt=""
-                        />
-                      </a>
-                      <div className="p-5">
-                        <a href="#">
-                          <h5 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
-                            {current.heading}
-                          </h5>
-                        </a>
-                        <p className="mb-3 text-sm text-gray-700 dark:text-gray-400">
-                          {current.details}
-                        </p>
-                        <a
-                          href="#"
-                          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300"
-                        >
-                          Read more
-                          <svg
-                            aria-hidden="true"
-                            className="w-4 h-4 ml-2 -mr-1"
-                            fill="currentColor"
-                            viewBox="0 0 20 20"
-                            xmlns="http://www.w3.org/2000/svg"
-                          >
-                            <path
-                              fillRule="evenodd"
-                              d="M10.293 3.293a1 1 0 011.414 0l6 6a1 1 0 010 1.414l-6 6a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-4.293-4.293a1 1 0 010-1.414z"
-                              clipRule="evenodd"
-                            ></path>
-                          </svg>
-                        </a>
-                      </div>
-                    </div>
+                    <SingleArticle index={index} current={current} />
                   ))}
                 </div>
                 <div className="flex justify-center mt-4">
