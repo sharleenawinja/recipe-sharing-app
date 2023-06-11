e53a399ca1faf8b468730604ebda880877e29766
sharleenawinja <awinjasharleen@gmail.com>
2023-06-11 12:33:24 +0300

    adding more sections to the navbar

--- src/components/layout/Navbar.component.tsx
+++ src/components/layout/Navbar.component.tsx
@@ -1,43 +1,12 @@
-import { BiSearchAlt, BiUserCircle } from "react-icons/bi";
+import {
+  BiBell,
+  BiHomeCircle,
+  BiSearchAlt,
+  BiUserCircle,
+} from "react-icons/bi";
 import { MdLogout } from "react-icons/md";
-
-const NotificationBell = () => {
-  return (
-    <div className="relative">
-      <p
-        className="pt-1 pr-1 pb-1 pl-1 bg-white text-gray-700 rounded-full transition-all duration-200
-          hover:text-gray-900 focus:outline-none hover:bg-gray-100 h-11"
-      >
-        <span className="justify-center items-center flex">
-          <span className="items-center justify-center flex">
-            <svg
-              className="w-6 h-12"
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              strokeWidth="2"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4
-                  0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6
-                  0H9"
-              />
-            </svg>
-          </span>
-        </span>
-      </p>
-      <p
-        className="px-1.5 py-0.5 font-semibold text-xs items-center bg-indigo-600 text-white rounded-full inline-flex
-          absolute -top-px -right-1"
-      >
-        2
-      </p>
-    </div>
-  );
-};
+import { TbFriends } from "react-icons/tb";
+import { AiOutlineMessage } from "react-icons/ai";
 
 const Navbar = () => {
   return (
@@ -58,8 +27,22 @@ const Navbar = () => {
             />
           </div>
           <div className="md:space-x-10 justify-end items-center ml-auto flex space-x-6 sm:mr-6 md:mr-10">
-            {/* clicking notification icon should take you to a page with all the notifications */}
-            <NotificationBell />
+            <div className="justify-center items-center flex flex-col relative">
+              <BiHomeCircle className="h-9 w-7" />
+              <p className="hidden md:block">Home</p>
+            </div>
+            <div className="justify-center items-center flex flex-col relative">
+              <TbFriends className="h-9 w-7" />
+              <p className="hidden md:block">Allies</p>
+            </div>
+            <div className="justify-center items-center flex flex-col relative">
+              <AiOutlineMessage className="h-9 w-7" />
+              <p className="hidden md:block">Messages</p>
+            </div>
+            <div className="justify-center items-center flex flex-col relative">
+              <BiBell className="h-9 w-7" />
+              <p className="hidden md:block">Notifications</p>
+            </div>
             <div className="justify-center items-center flex flex-col relative">
               <BiUserCircle className="h-9 w-9 rounded-full" />
               <p className="hidden md:block">User</p>
