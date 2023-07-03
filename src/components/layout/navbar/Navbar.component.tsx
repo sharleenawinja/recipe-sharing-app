f493fdcdfb1dcd92b524288931b1c793898f41a7
sharleenawinja <awinjasharleen@gmail.com>
2023-07-03 13:13:57 +0300

    working on 'create a post' modal

--- src/components/layout/navbar/Navbar.component.tsx
+++ src/components/layout/navbar/Navbar.component.tsx
@@ -37,7 +37,7 @@ const Navbar = () => {
 
   return (
     <>
-      <div className="w-full border-b-2 border-gray-200">
+      <div className="w-full">
         <div className="bg-white h-16 items-center mx-auto px-4 flex space-x-1 md:space-x-2 lg:space-x-8">
           <div className="flex-shrink-0 h-16 w-20 ml-0 md:w-30 lg:w-40 font-bold text-xs uppercase tracking-wide text-center p-2 bg-white border-2 border-black rounded-2xl shadow-md flex justify-center items-center">
             <h1>
@@ -60,25 +60,27 @@ const Navbar = () => {
             </button>
           </div>
           {isModalOpen && (
-            <div className="fixed inset-0 flex items-center justify-center bg-gray-800 bg-opacity-75">
-              <div className="flex space-x-4 absolute top-20 bg-white h-16 items-center mx-auto px-4 w-11/12 md:w-2/3 lg:w-1/2">
-                <input
-                  placeholder="Search for a recipe..."
-                  type="search"
-                  className="border border-gray-300 focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm w-full rounded-lg pl-10 pr-3 py-2"
-                />
-                <button
-                  className="text-gray-600 hover:text-gray-800 mr-0"
-                  onClick={() => setIsModalOpen(false)}
-                >
-                  Search
-                </button>
-                <button
-                  className="text-gray-600 hover:text-gray-800 mr-0"
-                  onClick={() => setIsModalOpen(false)}
-                >
-                  Close
-                </button>
+            <div className="w-full">
+              <div className="fixed inset-0 flex justify-center bg-gray-800 bg-opacity-75">
+                <div className="flex space-x-4 absolute top-20 bg-white h-16 items-center mx-auto px-4 w-11/12 md:w-2/3 lg:w-1/2">
+                  <input
+                    placeholder="Search for a recipe..."
+                    type="search"
+                    className="border border-gray-300 focus:ring-indigo-600 focus:border-indigo-600 sm:text-sm w-full rounded-lg pl-10 pr-3 py-2"
+                  />
+                  <button
+                    className="text-gray-600 hover:text-gray-800 mr-0"
+                    onClick={() => setIsModalOpen(false)}
+                  >
+                    Search
+                  </button>
+                  <button
+                    className="text-gray-600 hover:text-gray-800 mr-0"
+                    onClick={() => setIsModalOpen(false)}
+                  >
+                    Close
+                  </button>
+                </div>
               </div>
             </div>
           )}
--- src/components/layout/posts/NewPost.tsx
+++ src/components/layout/posts/NewPost.tsx
@@ -1,29 +1,97 @@
-import { AiOutlineFileAdd } from "react-icons/ai";
-import { BiCalendarEvent, BiImage, BiPencil, BiVideo } from "react-icons/bi";
+import { useState } from "react";
+import { AiOutlineComment, AiOutlineFileAdd } from "react-icons/ai";
+import {
+  BiCalendarEvent,
+  BiImage,
+  BiPencil,
+  BiUserCircle,
+  BiVideo,
+  BiXCircle,
+} from "react-icons/bi";
+import { BsUpload } from "react-icons/bs";
 
 const NewPost = () => {
+  const [isModalOpen, setIsModalOpen] = useState(false);
   return (
-    <div className="max-w-xl mx-auto bg-white rounded-lg shadow-md p-6 mt-3">
-      <div className="flex items-center space-x-4">
-        <h1 className="flex items-center">
-          Add a post <AiOutlineFileAdd className="ml-2" />
-        </h1>
+    <>
+      <div className="max-w-xl mx-auto bg-white rounded-lg shadow-md p-6 mt-3">
+        <div className="flex items-center space-x-4">
+          <button
+            className="flex items-center"
+            onClick={() => setIsModalOpen(true)}
+          >
+            Add a post <AiOutlineFileAdd className="ml-2" />
+          </button>
+        </div>
+        <div className="flex flex-wrap items-center mt-4 space-x-3 space-y-2">
+          <button
+            className="flex items-center ml-3 mt-2"
+            onClick={() => setIsModalOpen(true)}
+          >
+            <BiImage /> <span>Photo</span>
+          </button>
+          <button className="flex items-center">
+            <BiVideo /> <span>Video</span>
+          </button>
+          <button className="flex items-center">
+            <BiCalendarEvent /> <span>Event</span>
+          </button>
+          <button className="flex items-center">
+            <BiPencil /> <span>Article</span>
+          </button>
+        </div>
       </div>
-      <div className="flex flex-wrap items-center mt-4 space-x-3 space-y-2">
-        <button className="flex items-center ml-3 mt-2">
-          <BiImage /> <span>Photo</span>
-        </button>
-        <button className="flex items-center">
-          <BiVideo /> <span>Video</span>
-        </button>
-        <button className="flex items-center">
-          <BiCalendarEvent /> <span>Event</span>
-        </button>
-        <button className="flex items-center">
-          <BiPencil /> <span>Article</span>
-        </button>
-      </div>
-    </div>
+      {isModalOpen && (
+        <div className="w-full">
+          <div className="fixed inset-0 flex justify-center bg-gray-800 bg-opacity-75">
+            <div className="flex justify-center items-center h-screen w-full">
+              <div className="bg-white rounded-lg shadow-md p-6 w-2/5 h-1/2">
+                <div className="flex items-center justify-between">
+                  <h1 className="text-xl font-bold">Create a Post</h1>
+                  <button
+                    className="text-gray-500"
+                    onClick={() => setIsModalOpen(false)}
+                  >
+                    <BiXCircle className="text-xl" />
+                  </button>
+                </div>
+                <div className="flex items-center space-x-2 mt-4">
+                  <div>
+                    <BiUserCircle className="text-gray-500 text-3xl" />
+                  </div>
+                  <div>
+                    <p className="text-sm font-bold">user</p>
+                  </div>
+                </div>
+                <div className="mt-4">
+                  <textarea
+                    id="message"
+                    className="block p-2.5 w-full h-56 text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300"
+                    placeholder="Write your thoughts here..."
+                  ></textarea>
+                </div>
+                <div className="flex flex-wrap items-center mt-4 space-x-96 space-y-2">
+                  <div className="flex flex-wrap items-center space-x-3 space-y-2">
+                    <div className="flex items-center ml-3 mt-2">
+                      <BiImage /> <span>Photo</span>
+                    </div>
+                    <div className="flex items-center">
+                      <BiVideo /> <span>Video</span>
+                    </div>
+                    <div className="flex items-center">
+                      <AiOutlineComment /> <span>Anyone</span>
+                    </div>
+                  </div>
+                  <div className="flex items-center space-x-3">
+                    <BsUpload /> <span>Post</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      )}
+    </>
   );
 };
 
